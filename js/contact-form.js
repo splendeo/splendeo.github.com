2823c07811aae7636aae07704f9a0d086c85fa97
Enrique García <kikito@gmail.com>
2010-10-26 18:44:28 +0200

    making contact form send emails without changing window

---
+++ js/contact-form.js
@@ -0,0 +1,24 @@
+var submitted=false;
+
+function show_thank_you_if_submitted(){
+  if(submitted) {
+    $("#ss-form").fadeOut('slow', function(){ $('#thank-you').show(); });
+  }
+}
+
+function activate_contact_form_validation(){
+  $("#ss-form").validate();
+}
+
+function show_contact_form(href){
+  $.colorbox({ 
+    width:'60%',
+    href: href,
+    onComplete: activate_contact_form_validation
+  });
+}
+
+function mark_as_submitted(){
+  submitted=true;
+}
+
