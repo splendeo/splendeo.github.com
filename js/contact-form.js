41312ed65737d33dc4aebdab9f3991b781882e10
Enrique García <kikito@gmail.com>
2011-02-28 13:06:37 +0100

    adding missing files

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   // make the "contact us" left link show the javascript contact us form with an animation 
-  $("#dwp-contact-button").click(function() {
+  $(".open-contact").click(function() {
     $(".dwpcontact-page").animate({ left: parseInt($(".dwpcontact-page").css("left"),10) == 0 ? -354 :  0 });
     $("#dwp-contact-button").animate({ left: parseInt($("#dwp-contact-button").css("left"),10) == 0 ? 354 :  0 });
     return false;
